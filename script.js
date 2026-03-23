f9c9e87d2bd3b6b10ff45507a3515f655fd18f3e
giulia-buonasperanza <giulia.9402@gmail.com>
2026-03-23 17:43:45 +0100

    terza condizione per il prezzo

--- script.js
+++ script.js
@@ -13,18 +13,21 @@ const prezzoBaseBiglietto = kmInseritiNumero * 0.21;
 //prompt per chiedere l'età del passeggero
 
 const etàDelPasseggero = prompt('Inserisci la tua età');
-console.log(etàDelPasseggero +' '+'anni');
+console.log(etàDelPasseggero + ' ' + 'anni');
 const etàInseritaNumero = parseInt(etàDelPasseggero);
 
 //if l'età è compresa tra i 18 e i 65 il prezzo del biglietto è calcolato.
 //if else l'età è minore di 18 applica lo sconto del 20%
 //else se l'età è maggiore di 65 anni applica lo sconto del 40%.
 
-if( (etàInseritaNumero >= 18) && (etàInseritaNumero < 65) ) {
+if ((etàInseritaNumero >= 18) && (etàInseritaNumero < 65)) {
     let prezzoFinaleBiglietto = prezzoBaseBiglietto;
     console.log(`${prezzoFinaleBiglietto.toFixed(2)}€`);
-}else if (etàInseritaNumero < 18) {
-    let prezzoFinaleBiglietto = ((prezzoBaseBiglietto / 100)*80);
-     console.log(`${prezzoFinaleBiglietto.toFixed(2)}€`);
+} else if (etàInseritaNumero < 18) {
+    let prezzoFinaleBiglietto = ((prezzoBaseBiglietto / 100) * 80);
+    console.log(`${prezzoFinaleBiglietto.toFixed(2)}€`);
+// età>=65    
+} else { 
+    let prezzoFinaleBiglietto = ((prezzoBaseBiglietto / 100) * 60);
+    console.log(`${prezzoFinaleBiglietto.toFixed(2)}€`);
 }
-
